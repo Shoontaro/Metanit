14c2aac320fa604fcfbf693cc2d6bec1d0a84d4e
Yumo <Yumo@DESKTOP-ILCBMLQ>
2024-08-13 17:03:49 +0300

    Добавьте файлы проекта.

---
+++ AnonimMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿//MessageHandler handler = delegate (string mes)
+//{
+//    Console.WriteLine(mes);
+//    Console.ReadLine();
+//};
+//handler("hello world!");
+
+
+
+//delegate void MessageHandler(string message);
+
+
+
+//ShowMessage("hello!", delegate (string mes)
+//{
+//    Console.WriteLine(mes);
+//});
+
+//static void ShowMessage(string message, MessageHandler handler)
+//{
+//    handler(message);
+//}
+
+//delegate void MessageHandler(string message);
+
+//MessageHandler handler = delegate 
+//{
+//    Console.WriteLine("анонимный метод");
+//};
+//handler("hello world!");    // анонимный метод
+
+//delegate void MessageHandler(string message);
+
